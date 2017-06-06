6d115b7fbb07ed3ddd9982a322f6256231901ccf
CHUSS\Jesus <chuss424@hotmail.com>
2017-06-06 18:08:23 -0400

    remodelacion de la vista inicial de laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,9 @@
 <html>
     <head>
         <title>Laravel</title>
-        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.3/jquery.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
-        <script type="text/javascript" src="js/script.js"></script>
+
         <style>
             html, body {
                 height: 100%;
@@ -40,23 +37,9 @@
     </head>
     <body>
         <div class="container">
-            
-                <div class="row">
-                    <div class="col-md-6">
-                        <select class="form-control" onchange="buscar_ciudad()">
-                            <option>Estados</option>
-                            @foreach($Estados as $est)
-                            <!--<option value="{{$Estados->nombre}}">{{$Estados->nombre}}</option>-->
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-md-6">
-                        <select class="form-control" disabled="">
-                            <option>Ciudades</option>
-                        </select>
-                    </div>
-                </div>
-            
+            <div class="content">
+                <div class="title"><a href="{{url('ciudad')}}">Ir a seleccionar estado</a></div>
+            </div>
         </div>
     </body>
 </html>
